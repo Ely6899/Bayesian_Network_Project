611fa9c9dc7ec0ae4634e6a12e31e99c1485c5a7
Ely6899 <elikozinetz@protonmail.com>
2022-12-09 18:50:46 +0200

    Initial commit. Main implemented and documented.

---
+++ src/Ex1.java
@@ -0,0 +1,117 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Ex1 {
+
+
+    /**
+     * Filters out the variable names of a given probability query.
+     * Filtering is done using REGEX.
+     * @param query probability string query (Example: P(B=T|J=T,M=T)). Needs to be the exact format!
+     * @return String array of the query variable names.
+     */
+    private static String[] getQueryNames(String query){
+        List<String> stringList = new ArrayList<>();
+        Pattern pattern = Pattern.compile("([a-z A-Z\\d]*)=");
+        Matcher matcher = pattern.matcher(query);
+        while(matcher.find()){
+            stringList.add(matcher.group(1));
+        }
+        String[] namesArr = new String[stringList.size()];
+        return stringList.toArray(namesArr); //First element always the name element we wish to find. The rest are the parents.
+    }
+
+    /**
+     * Filters out the variable values given in the probability query.
+     * Filtering is done using REGEX.
+     * @param query probability string query (Example: P(B=T|J=T,M=T)). Needs to be the exact format!
+     * @return String array of the query variable names
+     */
+    private static String[] getQueryIndex(String query){
+        List<String> stringList = new ArrayList<>();
+        Pattern pattern = Pattern.compile("=([a-z A-Z\\d]*)");
+        Matcher matcher = pattern.matcher(query);
+        while(matcher.find()){
+            stringList.add(matcher.group(1));
+        }
+        String[] namesArr = new String[stringList.size()];
+        return stringList.toArray(namesArr);
+    }
+
+
+    /**
+     * Reads an input text file which needs to contain the xml file we read from,
+     * and it needs to contain probability queries followed by a number indicating the function used to
+     * calculate the probability of the given query.
+     * @param input Input text file. Must contain in the first line the xml file we wish to read from.
+     */
+    private static void readFromXmlInputFile(String input){
+        BufferedReader br;
+        BayesianNetwork bayesianNetwork;
+        try {
+            //let BufferedReader read the content of the given input file.
+            br = new BufferedReader(new FileReader("src/"+input));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            String line;
+            try {
+                line = br.readLine(); //Input file line reader.
+
+                //This is the first iteration of the line reader, reading the given xml file name.
+                //Parsing of the XML is done in BayesianNetwork class.
+                bayesianNetwork = new BayesianNetwork(line);
+                System.out.println(bayesianNetwork);
+                line = br.readLine();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            //Iterate until the end of the file.
+            while (line != null) {
+                try {
+                    char funcInput = line.charAt(line.length() - 1); //Function number input.
+                    String query = line.substring(0,line.length() - 2); //Query string input.
+
+                    //Switch given an input number.
+                    switch (funcInput){
+                        case '1':
+                            bayesianNetwork.func1(getQueryNames(query), getQueryIndex(query));
+                            break;
+                        case '2':
+                            bayesianNetwork.func2(getQueryNames(query), getQueryIndex(query));
+                            break;
+                        case '3':
+                            bayesianNetwork.func3(getQueryNames(query), getQueryIndex(query));
+                            break;
+                        default:
+                            System.out.println("Invalid input");
+                            break;
+                    }
+                    line = br.readLine();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        } finally {
+            try {
+                br.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    //Main will run the xml reading function, which will parse the input, the XML, and run throught the input file.
+    public static void main(String[] args) {
+        readFromXmlInputFile("input.txt");
+    }
+}
